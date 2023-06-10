244250ebb5d088587f503df5cca801b88f8dbb42
@Ash9311 <ashutoshmulki7@gmail.com>
2023-06-10 23:49:05 +0530

    signup,login,problems route implementation

--- index.js
+++ index.js
@@ -1,66 +1,160 @@
 const express = require('express')
 const app = express()
-const port = 3001
-
+const port = 3002
+var jwt = require("jsonwebtoken");
 const USERS = [];
-
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+const bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const cors = require("cors");
+app.use(cors());
+app.use(jsonParser);
+let USER_ID_COUNT = 1;
+const JWT_SECRET = "oursecret"
+const PROBLEMS = [
+  {
+    problemId: "1",
+    title: "401. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "42%",
+    description:
+      "Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.",
+    exampleIn: "left = 5, right = 7",
+    exampleOut: "4",
+  },
+  {
+    problemId: "2",
+    title: "205. Add two numbers",
+    difficulty: "Medium",
+    acceptance: "41%",
+    description:
+      "Given two numbers, add them and return them in integer range. use MOD=1e9+7",
+    exampleIn: "a = 100 , b = 200",
+    exampleOut: "300",
+  },
+  {
+    problemId: "3",
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+    description: "Write an algorithm to determine if a number n is happy.",
+    exampleIn: "n = 19",
+    exampleOut: "true",
+  },
+  {
+    problemId: "4",
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+    description: "Given number k , removed kth element",
+    exampleIn: "list: 1->2->3 , k=2",
+    exampleOut: "1->3",
+  },
+  {
+    problemId: "5",
+    title: "201. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "42%",
+    description:
+      "Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.",
+    exampleIn: "left = 5, right = 7",
+    exampleOut: "4",
+  },
+  {
+    problemId: "6",
+    title: "205. Add two numbers",
+    difficulty: "Medium",
+    acceptance: "41%",
+    description:
+      "Given two numbers, add them and return them in integer range. use MOD=1e9+7",
+    exampleIn: "a = 100 , b = 200",
+    exampleOut: "300",
+  },
+  {
+    problemId: "7",
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+    description: "Write an algorithm to determine if a number n is happy.",
+    exampleIn: "n = 19",
+    exampleOut: "true",
+  },
+  {
+    problemId: "8",
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+    description: "Given number k , removed kth element",
+    exampleIn: "list: 1->2->3 , k=2",
+    exampleOut: "1->3",
+  },
+];
 
 
 const SUBMISSION = [
 
 ]
 
-app.post('/signup', function(req, res) {
+app.post('/signup', function (req, res) {
   // Add logic to decode body
   // body should have email and password
-
-
+  const email = req.body.email;
+  const password = req.body.password;
+  if (USERS.find((x) => { x.email === email })) {
+    return res.status(403).json({ msg: "Email already exists" });
+  }
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
+  USERS.push({ email, password, id: USER_ID_COUNT++ })
   // return back 200 status code to the client
-  res.send('Hello World!')
+  return res.status(200).json({ msg: "Success" });
 })
 
-app.post('/login', function(req, res) {
+app.post('/login', function (req, res) {
   // Add logic to decode body
   // body should have email and password
-
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(password)
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
+  const user = USERS.find((x) => { x.email == email });
+  if (!user) {
+    return res.status(403).json({ msg: "user not found" })
+  }
+  // If the password is not the same, return back 401 status code to the client
+  if (user.password !== password) {
+    return res.status(403).json({ msg: "incorrect password" })
+  }
 
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  const token = jwt.sign(
+    { id: user.id }, JWT_SECRET
+  );
+  return res.json({ token })
 })
 
-app.get('/questions', function(req, res) {
-
+app.get('/problems', function (req, res) {
+ const filteredProblems = PROBLEMS.map((problem)=>({
+  problemId:problem.problemId,
+   title: problem.title,
+    difficulty: problem.difficulty,
+    acceptance: problem.acceptance
+ })
+ )
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
+  res.json({problems:filteredProblems})
+});
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
+app.get("/submissions", function (req, res) {
+  // return the users submissions for this problem
   res.send("Hello World from route 4!")
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
+app.post("/submissions", function (req, res) {
+  // let the user submit a problem, randomly accept or reject the solution
+  // Store the submission in the SUBMISSION array above
   res.send("Hello World from route 4!")
 });
 
@@ -68,6 +162,6 @@ app.post("/submissions", function(req, res) {
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Example app listening on port ${port}`)
 })
